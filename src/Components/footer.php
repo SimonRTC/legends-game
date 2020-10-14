9ea963e1feef11726c49dc8eb309ddd87c0d73db
SimonRTC <simon.malpel@orange.fr>
2020-10-14 20:58:37 +0200

    update(UI): Update bootstrap

--- src/Components/footer.php
+++ src/Components/footer.php
@@ -1,6 +1,5 @@
 
-
-        <script src="/assets/js/bootstrap.min.js"></script>
-        <script src="/assets/js/bootstrap.bundle.min.js"></script>
+        <script type="text/javascript" src="/assets/js/jquery-3.5.1.min.js"></script>
+        <script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
     </body>
 </html> 
