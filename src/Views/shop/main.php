d2b0c9c213dca73a66fe714fd3ca8a314942b998
Sibel Urbain <sibel.urbain@gmail.com>
2020-10-16 15:48:38 +0200

    update(shop): Shop UI

--- src/Views/shop/main.php
+++ src/Views/shop/main.php
@@ -1,1 +1,127 @@
-Hello world
+<div class="overlay overlay-shop">
+	<div class="fadeout">
+		<div class="container">
+			<div class="col-md-6 p-lg-5 mx-auto my-5">
+				<h1>Boutique <span class="text-warning">Legends</span></h1>
+				<p>Achetez de nouveaux équipement pour devenir la légende la plus stylée de tous les temps !</p>
+			</div>
+		</div>
+	</div>
+</div>
+<main class="bg-parchment" role="main">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-3">
+                <div class="list-group">
+                <div class="gold-deco"></div>
+                    <a href="#" class="btn btn-parchment">Guerrier</a>
+                    <a href="#" class="btn btn-parchment">Mage</a>
+                    <a href="#" class="btn btn-parchment">Archer</a>
+                </div>
+            </div>
+            <div class="col-lg-9">
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <h4 class="card-title">
+                                <a href="#">Protège-tête d'archer épique</a>
+                            </h4>
+                            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                            <div class="card-body">
+                                <h5><div class="coin-deco"></div>100g</h5> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <h4 class="card-title">
+                                <a href="#">Protège-tête d'archer épique</a>
+                            </h4>
+                            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                            <div class="card-body">
+                                <h5><div class="coin-deco"></div>100g</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <h4 class="card-title">
+                                <a href="#">Protège-tête d'archer épique</a>
+                            </h4>
+                            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                            <div class="card-body">
+                                <h5><div class="coin-deco"></div>100g</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <h4 class="card-title">
+                                <a href="#">Protège-tête d'archer épique</a>
+                            </h4>
+                            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                            <div class="card-body">
+                                <h5><div class="coin-deco"></div>100g</h5> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <h4 class="card-title">
+                                <a href="#">Protège-tête d'archer épique</a>
+                            </h4>
+                            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                            <div class="card-body">
+                                <h5><div class="coin-deco"></div>100g</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <h4 class="card-title">
+                                <a href="#">Protège-tête d'archer épique</a>
+                            </h4>
+                            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                            <div class="card-body">
+                                <h5><div class="coin-deco"></div>100g</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <h4 class="card-title">
+                                <a href="#">Protège-tête d'archer épique</a>
+                            </h4>
+                            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                            <div class="card-body">
+                                <h5><div class="coin-deco"></div>100g</h5> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <h4 class="card-title">
+                                <a href="#">Protège-tête d'archer épique</a>
+                            </h4>
+                            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                            <div class="card-body">
+                                <h5><div class="coin-deco"></div>100g</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6 mb-4">
+                        <div class="card h-100">
+                            <h4 class="card-title">
+                                <a href="#">Protège-tête d'archer épique</a>
+                            </h4>
+                            <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
+                            <div class="card-body">
+                                <h5><div class="coin-deco"></div>100g</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
