a470fd6b1e6ae31b71532d70c6cc6ebd0e0d8933
Axel Guihard <axelg-44@hotmail.fr>
2020-12-07 11:09:20 +0100

    fix(profile): Src fix

--- src/Views/profile.php
+++ src/Views/profile.php
@@ -26,10 +26,10 @@
         </div>
     </div>
     <div class="container-house-profile">
-        <img class="house-profile" src="assets/img/profile/house-profile.png" alt="">
+        <img class="house-profile" src="/assets/img/profile/house-profile.png" alt="">
     </div>
     <div class="container-tree-profile">
-        <img class="tree-profile" src="assets/img/profile/tree-profile.png" alt="">
+        <img class="tree-profile" src="/assets/img/profile/tree-profile.png" alt="">
     </div>
     <div class="container profile-container-2">
         <div class="row justify-content-center">
@@ -51,24 +51,24 @@
                 </div>
             </div>
             <div class="co-xl-12 col-lg-12 col-md-12 col-sm-4 mb-4 d-flex justify-content-center">
-                <img class="image-player-profile " src="assets/img/profile/fake-image.png" alt="Player image">
+                <img class="image-player-profile " src="/assets/img/profile/fake-image.png" alt="Player image">
             </div>
             <div class="skin-inventory-profile col-xl-7 col-lg-7 col-md-7 col-sm-9 col-9">
                 <h2 class="skin-profile-title">Skin</h2>
                 <div class="mb-2">
-                    <img class="skin-img-profile" src="assets/img/profile/fake-skin-image.png" alt="Fake skin image">
-                    <img class="skin-img-profile" src="assets/img/profile/fake-skin-image.png" alt="Fake skin image">
-                    <img class="skin-img-profile" src="assets/img/profile/fake-skin-image.png" alt="Fake skin image">
+                    <img class="skin-img-profile" src="/assets/img/profile/fake-skin-image.png" alt="Fake skin image">
+                    <img class="skin-img-profile" src="/assets/img/profile/fake-skin-image.png" alt="Fake skin image">
+                    <img class="skin-img-profile" src="/assets/img/profile/fake-skin-image.png" alt="Fake skin image">
                 </div>
                 <div class="mb-2">
-                    <img class="skin-img-profile" src="assets/img/profile/fake-skin-image.png" alt="Fake skin image">
-                    <img class="skin-img-profile" src="assets/img/profile/fake-skin-image.png" alt="Fake skin image">
-                    <img class="skin-img-profile" src="assets/img/profile/fake-skin-image.png" alt="Fake skin image">
+                    <img class="skin-img-profile" src="/assets/img/profile/fake-skin-image.png" alt="Fake skin image">
+                    <img class="skin-img-profile" src="/assets/img/profile/fake-skin-image.png" alt="Fake skin image">
+                    <img class="skin-img-profile" src="/assets/img/profile/fake-skin-image.png" alt="Fake skin image">
                 </div>
                 <div class="mb-2">
-                    <img class="skin-img-profile" src="assets/img/profile/fake-skin-image.png" alt="Fake skin image">
-                    <img class="skin-img-profile" src="assets/img/profile/fake-skin-image.png" alt="Fake skin image">
-                    <img class="skin-img-profile" src="assets/img/profile/fake-skin-image.png" alt="Fake skin image">
+                    <img class="skin-img-profile" src="/assets/img/profile/fake-skin-image.png" alt="Fake skin image">
+                    <img class="skin-img-profile" src="/assets/img/profile/fake-skin-image.png" alt="Fake skin image">
+                    <img class="skin-img-profile" src="/assets/img/profile/fake-skin-image.png" alt="Fake skin image">
                 </div>
             </div>
         </div>
